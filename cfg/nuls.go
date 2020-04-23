2b5bc781b8a01ee5ffc378f35cc62493bae27877
niels1286 <niels0519@gmail.com>
2020-04-23 16:21:33 +0800

    修改主网配置

--- cfg/nuls.go
+++ cfg/nuls.go
@@ -3,17 +3,18 @@
 // @Author  Niels  2020/4/18
 package cfg
 
-const DefaultChainId uint16 = 2      //account.NULSChainId
-const DefaultAddressPrefix = "tNULS" //account.NULSPrefix
-//const DefaultChainId uint16 =  account.NULSChainId
-//const DefaultAddressPrefix = account.NULSPrefix
+import "github.com/niels1286/nuls-go-sdk/account"
 
-// blackhole address
-const BlackHoleAddress = "tNULSeBaMhZnRteniCy3UZqPjTbnWKBPHX1a5d"
-
-//const APIServeURL = "https://api.nuls.io/jsonrpc/"
-//const PublicSercServeURL = "https://public1.nuls.io"
-const APIServeURL = "http://beta.api.nuls.io/jsonrpc/"
-const PublicSercServeURL = "https://beta.wallet.nuls.io/api"
+const DefaultChainId = account.NULSChainId
+const DefaultAddressPrefix = account.NULSPrefix
+const BlackHoleAddress = "NULSd6HgWSU1iR6BfNoQi85mAMT52JMFzpnok"
+const APIServeURL = "https://api.nuls.io/jsonrpc/"
+const PublicSercServeURL = "https://public1.nuls.io"
 
 const POCLockValue uint64 = 18446744073709551615
+
+//const DefaultChainId uint16 = 2
+//const DefaultAddressPrefix = "tNULS"
+//const BlackHoleAddress = "tNULSeBaMhZnRteniCy3UZqPjTbnWKBPHX1a5d"
+//const APIServeURL = "http://beta.api.nuls.io/jsonrpc/"
+//const PublicSercServeURL = "https://beta.wallet.nuls.io/api"
